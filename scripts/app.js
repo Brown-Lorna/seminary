c7b99e0143e55c0770fe26813d6780f7a7eca7f6
Travis CI <seminary@realgt.com>
2016-04-11 18:43:19 +0000

    Deploy to GitHub Pages from Travis CI

---
+++ scripts/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var seminaryApp = angular.module("seminaryApp", [
+    "ngRoute",
+    "angular.filter",
+    "firebase",
+    "seminaryControllers",
+    "seminaryServices"
+]);
+
+seminaryApp.config(['$routeProvider',
+  function($routeProvider) {
+	$routeProvider.
+		when('/lessons', {
+			templateUrl: 'views/lesson-list.html',
+			controller: 'LessonListController'
+		}).
+		when('/lessons/create', {
+            templateUrl: 'views/lesson-detail.html',
+            controller: 'LessonDetailController'
+        }).
+		when('/lessons/:lessonId', {
+			templateUrl: 'views/lesson-detail.html',
+			controller: 'LessonDetailController'
+		}).
+		when('/teams', {
+            templateUrl: 'views/team-list.html',
+            controller: 'TeamListController'
+        }).
+        when('/teams/:teamId', {
+            templateUrl: 'views/team-detail.html',
+            controller: 'TeamDetailController'
+        }).
+        when('/scripturemastery', {
+            templateUrl: 'views/scripturemastery.html',
+            controller: 'ScriptureMasteryController'
+        }).
+        when('/help', {
+            templateUrl: 'views/help.html'
+        }).
+		when('/index', {
+			templateUrl: 'views/seminary-index.html',
+			controller: 'SeminaryIndexController'
+		}).
+		otherwise({
+	        redirectTo: '/index'
+	      });
+}]);
